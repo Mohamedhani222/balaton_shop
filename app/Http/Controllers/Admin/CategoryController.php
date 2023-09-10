85ec0acc4d34154b20d08a7a8fc951acc09779aa
Mohamedhani222 <mohamedhacker740@gmail.com>
2023-09-11 02:14:02 +0300

    updates

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -29,7 +29,7 @@ class CategoryController extends Controller
     public function create()
     {
         $data['route'] = 'categories';
-        return view('admin.Category.create', $data);
+        return view('admin.category.create', $data);
     }
 
     /**
